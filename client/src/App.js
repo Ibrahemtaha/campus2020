a1a3b46a5fb8a2e1ce28cb1ce045652d4a8b4e6e
Ibrahem <bremoo87@hotmail.com>
2020-03-23 12:31:53 +0200

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
@@ -9,14 +10,16 @@ import Error from "./pages/Error";
 
 function App() {
   return (
-    <>
-      <Home />
-      <About />
-      <Login />
-      <Contact />
-      <Admin />
-      <Error />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/About" component={About} />
+        <Route exact path="/Contact" component={Contact} />
+        <Route exact path="/Login" component={Login} />
+        <Route exact path="/Admin" component={Admin} />
+        <Route exact path="/Error" component={Error} />
+      </Switch>
+    </Router>
   );
 }
 
